38f23ecd99b0bb87f2a0eddd42f13fb9a89f7e32
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-08-15 20:22:49 +0300

    create react app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Products from './Components/products';
 
 ReactDOM.render(
   <React.StrictMode>
