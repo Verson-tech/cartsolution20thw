3c6dc098f30a2fc5af5b047554212566c541c9e5
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-08-19 18:58:34 +0300

    commented out unused items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Products from './Components/products';
+// import Products from './Components/products';
 
 ReactDOM.render(
   <React.StrictMode>
